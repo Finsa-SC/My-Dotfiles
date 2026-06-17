e831219a07d954bb3ba4cfcd6a2e1257cd0fa85f
Finsa-SC <finsakusumaputra@gmail.com>
2026-06-17 15:24:09 +0700

    fix(sandbox): user path live for sandbox terminal

--- my-rice/.config/quickshell/processes/terminal-sandbox.sh
+++ my-rice/.config/quickshell/processes/terminal-sandbox.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-DOTFILES="$(cd "$SCRIPT_DIR/../../.." && pwd)"
-
 exec kitty --detach --title="Sandbox" -- podman run --rm -it \
   --volume "$HOME:/home/$USER/real:ro" \
-  --volume "$DOTFILES/my-rice/.config/fish-sandbox/fish/config.fish:/root/.config/fish/config.fish:ro" \
-  --volume "$DOTFILES/my-rice/.config/fish-sandbox/ascii.txt:/root/.config/fish-sandbox/ascii.txt:ro" \
+  --volume "$HOME/.config/fish-sandbox/fish/config.fish:/root/.config/fish/config.fish:ro" \
+  --volume "$HOME/.config/fish-sandbox/ascii.txt:/root/.config/fish-sandbox/ascii.txt:ro" \
   localhost/kali-fish \
   fish

8dc87f0569fc82ce78959db6f383848ec405455f
Finsa-SC <finsakusumaputra@gmail.com>
2026-06-18 13:14:10 +0700

    fix(rules): fix warning in rules window

--- my-rice/.config/hypr-custom/modules/rules.lua
+++ my-rice/.config/hypr-custom/modules/rules.lua
@@ -1,15 +1,11 @@
 hl.on("hyprland.start", function()
-    -- Rule dasar yang pakai tabel (untuk float, pin, size)
     hl.window_rule({
         match = { title = "^(Picture-in-Picture)$" },
         float = true,
         pin = true,
-        size = "400 225"
+        size = "400 225" -- Jika pakai spasi aman di dispatch dasar, silakan dipertahankan
     })
 
-    -- Tembak rule dekorasi langsung pakai dispatcher mentah
-    -- Menggunakan syntax asli Hyprland: windowrulev2 = rule, kriteria
     hl.dispatch("windowrulev2", "opaque, title:^(Picture-in-Picture)$")
-    hl.dispatch("windowrulev2", "alphaoverride 1.0 1.0, title:^(Picture-in-Picture)$")
     hl.dispatch("windowrulev2", "noblur, title:^(Picture-in-Picture)$")
 end)

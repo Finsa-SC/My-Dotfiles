d7c15f7d765e1320ada3a14cd14211f38c8b6252
Finsa-SC <finsakusumaputra@gmail.com>
2026-08-10 16:53:48 +0700

    refactor(general): change border color from default -> catppuccin blue

--- my-rice/.config/hypr-custom/modules/general.lua
+++ my-rice/.config/hypr-custom/modules/general.lua
@@ -3,6 +3,8 @@ hl.config({
         gaps_in     = gapsIn,
         gaps_out    = gapsOut,
         border_size = borderSize,
+        ["col.active_border"]   = "rgba(89b4faff)",   -- Catppuccin Mocha Blue (active)
+        ["col.inactive_border"] = "rgba(6c7086ff)",   -- Catppuccin Mocha Overlay0 (inactive)
     },
     binds = {
         allow_pin_fullscreen = false,
